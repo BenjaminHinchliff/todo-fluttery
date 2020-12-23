3e136045c52e20cb951798b381acd2f98decf63c
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-12-22 23:42:48 -0800

    split adding todo page to separate file

---
+++ lib/add_todo_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'todo.dart';
+
+class AddTodoPage extends StatefulWidget {
+  AddTodoPage({Key key}) : super(key: key);
+
+  @override
+  _AddTodoPageState createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Todo'),
+        actions: [
+          Padding(
+              padding: EdgeInsets.only(right: 10, top: 10, bottom: 10),
+              child: IconButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Navigator.of(context).pop(Todo(
+                        name: _nameController.text,
+                        priority: TodoPriority.low));
+                  }
+                },
+                icon: Icon(Icons.done),
+              ))
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Add Name",
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a name';
+                    }
+                    return null;
+                  },
+                  controller: _nameController,
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 import 'todo.dart';
+import 'add_todo_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,68 +17,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(title: 'Todo App'),
-    );
-  }
-}
-
-class AddTodoPage extends StatefulWidget {
-  AddTodoPage({Key key}) : super(key: key);
-
-  @override
-  _AddTodoPageState createState() => _AddTodoPageState();
-}
-
-class _AddTodoPageState extends State<AddTodoPage> {
-  final _formKey = GlobalKey<FormState>();
-  final _nameController = TextEditingController();
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Todo'),
-        actions: [
-          Padding(
-              padding: EdgeInsets.only(right: 10, top: 10, bottom: 10),
-              child: IconButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    Navigator.of(context).pop(Todo(
-                        name: _nameController.text,
-                        priority: TodoPriority.low));
-                  }
-                },
-                icon: Icon(Icons.done),
-              ))
-        ],
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Add Name",
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter a name';
-                    }
-                    return null;
-                  },
-                  controller: _nameController,
-                ),
-              ],
-            )),
-      ),
     );
   }
 }
